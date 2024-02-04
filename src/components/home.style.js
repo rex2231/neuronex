8003a1234391504061e042bf18e7ee05980a4ba5
sheninthjr <sheninthjr23@gmail.com>
2024-02-04 15:56:14 +0530

    Overflow X hidden

--- src/components/home.style.js
+++ src/components/home.style.js
@@ -3,11 +3,12 @@ import styled from "styled-components";
 // For Scrolling Effect
 
 export const Wrapper = styled.div`
-    scroll-snap-type: y mandatory;
-    overflow-y: scroll;
-    height: 100vh;
+  scroll-snap-type: y mandatory;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  height: 100vh;
 `;
 
 export const Content = styled.div`
-    scroll-snap-align: start;
+  scroll-snap-align: start;
 `;
